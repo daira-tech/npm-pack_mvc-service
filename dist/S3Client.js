e6ccd1e26a574d81f1e47b89db2b7ee02e8bd21a
ndaira <nakadaira44@yahoo.co.jp>
2025-05-14 19:00:17 +0900

    fix s3 url

--- dist/S3Client.js
+++ dist/S3Client.js
@@ -16,7 +16,7 @@ const aws_sdk_1 = __importDefault(require("aws-sdk"));
 const Base64Client_1 = __importDefault(require("./Base64Client"));
 class S3Client {
     get urlPrefix() {
-        return `https://${this.bucketName}.${this.region}`;
+        return `https://${this.bucketName}.s3.${this.region}.amazonaws.com`;
     }
     constructor() {
         this.bucketName = process.env.S3_BUCKET_NAME;
@@ -29,6 +29,9 @@ class S3Client {
             secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
             region: this.region
         });
+    }
+    makeKey(path, fileName) {
+        return `${path.replace(/^\/|\/$/g, '')}/${fileName}`;
     }
     url(path, fileName = '') {
         path = path.replace(/^\/|\/$/g, '');
@@ -43,10 +46,9 @@ class S3Client {
     }
     uploadJson(path, fileName, data) {
         return __awaiter(this, void 0, void 0, function* () {
-            path = path.replace(/^\/|\/$/g, '');
             yield this.client.putObject({
                 Bucket: this.bucketName,
-                Key: `${path}/${fileName}`,
+                Key: this.makeKey(path, fileName),
                 Body: JSON.stringify(data),
                 ContentType: 'application/json',
             }).promise();
@@ -56,13 +58,55 @@ class S3Client {
         return __awaiter(this, void 0, void 0, function* () {
             const base64Client = new Base64Client_1.default();
             const mergedPdfBase64 = yield base64Client.mergeToPdfBase64(base64Datas);
-            path = path.replace(/^\/|\/$/g, '');
             yield this.client.putObject({
                 Bucket: this.bucketName,
-                Key: `${path}/${fileName}`,
+                Key: this.makeKey(path, fileName),
                 Body: Buffer.from(mergedPdfBase64, 'base64'),
                 ContentType: 'application/pdf',
             }).promise();
+        });
+    }
+    uploadText(path, fileName, text) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.client.putObject({
+                Bucket: this.bucketName,
+                Key: this.makeKey(path, fileName),
+                Body: text,
+                ContentType: 'text/plain',
+            }).promise();
+        });
+    }
+    uploadStackText(path, fileName, text) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let preText = yield this.getText(path, fileName);
+            if (typeof preText === 'string') {
+                text = preText + '\n' + text;
+            }
+            yield this.uploadText(path, fileName, text);
+        });
+    }
+    getText(path, fileName) {
+        return __awaiter(this, void 0, void 0, function* () {
+            var _a;
+            try {
+                const res = yield this.client.getObject({
+                    Bucket: this.bucketName,
+                    Key: this.makeKey(path, fileName),
+                }).promise();
+                if (res.Body === undefined) {
+                    throw new Error(`Failed to get text data. Response body is undefined.`);
+                }
+                if (((_a = res.ContentType) === null || _a === void 0 ? void 0 : _a.startsWith('text/')) === false) {
+                    throw new Error(`Cannot get text data from non-text file. ContentType: ${res.ContentType}`);
+                }
+                return res.Body.toString('utf-8');
+            }
+            catch (ex) {
+                if (ex instanceof Error && ex.name === 'NoSuchKey') {
+                    return null;
+                }
+                throw ex;
+            }
         });
     }
 }
--- src/S3Client.ts
+++ src/S3Client.ts
@@ -1,12 +1,13 @@
 import AWS from 'aws-sdk';
 import Base64Client from './Base64Client';
+import { error } from 'pdf-lib';
 
 export default class S3Client {
     private client: AWS.S3;
     private readonly bucketName = process.env.S3_BUCKET_NAME as string;
     private readonly region = process.env.S3_REGION as string;
     get urlPrefix(): string {
-        return `https://${this.bucketName}.${this.region}`;
+        return `https://${this.bucketName}.s3.${this.region}.amazonaws.com`;
     }
 
     constructor() {
@@ -19,6 +20,10 @@ export default class S3Client {
             secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
             region: this.region
         });
+    }
+
+    private makeKey(path: string, fileName: string): string {
+        return `${path.replace(/^\/|\/$/g, '')}/${fileName}`;
     }
 
     public url(path: string, fileName: string = '') {
@@ -35,10 +40,9 @@ export default class S3Client {
     }
 
     public async uploadJson(path: string, fileName: string, data: {[key: string]: any}) {
-        path = path.replace(/^\/|\/$/g, '');
         await this.client.putObject({
             Bucket: this.bucketName,
-            Key: `${path}/${fileName}`,
+            Key: this.makeKey(path, fileName),
             Body: JSON.stringify(data),
             ContentType: 'application/json',
         }).promise();
@@ -48,12 +52,52 @@ export default class S3Client {
         const base64Client = new Base64Client();
         const mergedPdfBase64 = await base64Client.mergeToPdfBase64(base64Datas);
 
-        path = path.replace(/^\/|\/$/g, '');
         await this.client.putObject({
             Bucket: this.bucketName,
-            Key: `${path}/${fileName}`,
+            Key: this.makeKey(path, fileName),
             Body: Buffer.from(mergedPdfBase64, 'base64'),
             ContentType: 'application/pdf',
         }).promise();
+    }
+
+    public async uploadText(path: string, fileName: string, text: string) {
+        await this.client.putObject({
+            Bucket: this.bucketName,
+            Key: this.makeKey(path, fileName),
+            Body: text,
+            ContentType: 'text/plain',
+        }).promise();
+    }
+
+    public async uploadStackText(path: string, fileName: string, text: string) {
+        let preText = await this.getText(path, fileName);
+        if (typeof preText === 'string') {
+            text = preText + '\n' + text;
+        }
+
+        await this.uploadText(path, fileName, text);
+    }
+
+    public async getText(path: string, fileName: string) : Promise<string | null> {
+        try {
+            const res = await this.client.getObject({
+                Bucket: this.bucketName,
+                Key: this.makeKey(path, fileName),
+            }).promise();
+
+            if (res.Body === undefined) {
+                throw new Error(`Failed to get text data. Response body is undefined.`);
+            }
+
+            if (res.ContentType?.startsWith('text/') === false) {
+                throw new Error(`Cannot get text data from non-text file. ContentType: ${res.ContentType}`);
+            }
+            return res.Body.toString('utf-8');
+        } catch (ex: unknown) {
+            if (ex instanceof Error && ex.name === 'NoSuchKey') {
+                return null;
+            }
+            throw ex;
+        }
     }
 }
